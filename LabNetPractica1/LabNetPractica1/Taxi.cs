fd63f0de63b3f5affff16d213d6ccacd923949ef
Miguelch7 <chanampamiguel@gmail.com>
2023-03-04 15:15:55 -0300

    Creacion clases principales
    Se crearon las clases TransportePublico, Omnibus y Taxi

---
+++ LabNetPractica1/LabNetPractica1/Taxi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNetPractica1
+{
+    public class Taxi : TransportePublico
+    {
+        public Taxi(int pasajeros) : base(pasajeros)
+        {
+            this.Pasajeros = pasajeros;
+        }
+
+        public override string Avanzar()
+        {
+            return $"El taxi avanzó con { this.Pasajeros } pasajeros.";
+        }
+
+        public override string Detenerse()
+        {
+            return $"El taxi se detuvo con { this.Pasajeros } pasajeros.";
+        }
+    }
+}
---
+++ LabNetPractica1/LabNetPractica1/TransportePublico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNetPractica1
+{
+    public abstract class TransportePublico
+    {
+        public int Pasajeros { get; set; }
+
+        public TransportePublico(int pasajeros) 
+        {
+            this.Pasajeros = pasajeros;
+        }
+
+        public abstract string Avanzar();
+        public abstract string Detenerse();
+    }
+}
