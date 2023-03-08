ca0aaac11ffd13a81431a948839808d99af3cf3b
Miguelch7 <chanampamiguel@gmail.com>
2023-03-08 15:56:05 -0300

    Punto 1 - DividirPorCero()
    Se crea un método que arroja una excepción al dividir por cero.

---
+++ LabNetPractica2/LabNetPractica2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNetPractica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== Punto 1) ==========");
+
+            try
+            {
+                DividirPorCero();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("La operación finalizó");
+            }
+        }
+
+        static int DividirPorCero()
+        {
+            Console.Write("Ingrese un valor: ");
+            string valor = Console.ReadLine();
+            int valorNum;
+
+            while (!Int32.TryParse(valor, out valorNum))
+            {
+                Console.WriteLine("Debes ingresar un valor númerico!!!");
+                Console.Write("Ingrese un valor: ");
+                valor = Console.ReadLine();
+            }
+
+            return valorNum / 0;
+        }        
+    }
+}
