af3515db06e276c67f8eb0d7944365303f1a9cd3
Miguelch7 <chanampamiguel@gmail.com>
2023-03-12 12:29:49 -0300

    Listando categorias
    Se creó la logica para traer las categorias de la BDD y listarlas en pantalla.

---
+++ Lab.EF/Lab.EF.Logic/CategoriesLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab.EF.Data;
+using Lab.EF.Entities;
+
+namespace Lab.EF.Logic
+{
+    public class CategoriesLogic
+    {
+        protected readonly NorthwindContext _context;
+
+        public CategoriesLogic()
+        {
+            _context = new NorthwindContext();
+        }
+
+        public List<Categories> GetAll()
+        {
+            return _context.Categories.ToList();
+        }
+    }
+}
--- Lab.EF/Lab.EF.UI/Program.cs
+++ Lab.EF/Lab.EF.UI/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lab.EF.Entities;
+using Lab.EF.Logic;
 
 namespace Lab.EF.UI
 {
@@ -10,6 +12,19 @@ namespace Lab.EF.UI
     {
         static void Main(string[] args)
         {
+            CategoriesLogic categoriesLogic = new CategoriesLogic();
+
+            List<Categories> ListadoCategorias = categoriesLogic.GetAll();
+
+            Console.WriteLine("========== Listado de categorías ==========\n");
+
+            foreach (Categories c in ListadoCategorias)
+            {
+                Console.WriteLine($"Categoría: {c.CategoryName}");
+                Console.WriteLine($"Descripción: {c.Description}\n");
+            }
+
+            Console.ReadKey();
         }
     }
 }
