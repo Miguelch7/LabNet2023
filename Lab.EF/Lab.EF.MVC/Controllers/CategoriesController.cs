c7c2b66b99121eff94042d349e2f66dc1b20415d
Miguelch7 <chanampamiguel@gmail.com>
2023-03-22 11:12:59 -0300

    Crear y actualizar Categorias
    Se creó un formulario para crear/editar categorias

--- Lab.EF/Lab.EF.MVC/Controllers/CategoriesController.cs
+++ Lab.EF/Lab.EF.MVC/Controllers/CategoriesController.cs
@@ -25,6 +25,66 @@ namespace Lab.EF.MVC.Controllers
             return View(categories);
         }
 
+        public ActionResult Add()
+        {
+            ViewData["Action"] = "Add";
+            ViewData["BtnText"] = "Crear categoría";
+
+            return View("FormCategory", new CategoriesView() );
+        }
+
+        [HttpPost]
+        public ActionResult Add(CategoriesView categoryView)
+        {
+            CategoriesLogic categoriesLogic = new CategoriesLogic();
+
+            Categories category = new Categories()
+            {
+                CategoryName = categoryView.CategoryName, 
+                Description = categoryView.Description
+            };
+
+            categoriesLogic.Add(category);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int id)
+        {
+            CategoriesLogic categoriesLogic = new CategoriesLogic();
+
+            Categories category = categoriesLogic.GetById(id);
+
+            CategoriesView categoryView = new CategoriesView
+            {
+                Id = category.CategoryID,
+                CategoryName = category.CategoryName,
+                Description = category.Description
+            };
+
+            ViewData["Action"] = "Update";
+            ViewData["BtnText"] = "Guardar Cambios";
+
+            return View("FormCategory", categoryView);
+        }
+
+        [HttpPost]
+        public ActionResult Update(CategoriesView categoryView)
+        {
+            CategoriesLogic categoriesLogic = new CategoriesLogic();
+
+            Categories category = new Categories()
+            {
+                CategoryID = categoryView.Id,
+                CategoryName = categoryView.CategoryName,
+                Description = categoryView.Description
+            };
+
+            categoriesLogic.Update(category);
+
+            return RedirectToAction("Index");
+        }
+
         public ActionResult Delete(int id)
         {
             CategoriesLogic categoriesLogic = new CategoriesLogic();
