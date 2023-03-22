7b38ca889d8ec60c9197fe49bbdb1af9dd5a9c06
Miguelch7 <chanampamiguel@gmail.com>
2023-03-22 10:51:50 -0300

    Crear CategoriesController y listar categorias
    Se creo el controller de categoria, un viewmodel de categoria y se añadió el link de acceso al index de categoria desde el layout.

---
+++ Lab.EF/Lab.EF.MVC/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab.EF.Entities;
+using Lab.EF.Logic;
+using Lab.EF.MVC.Models;
+
+namespace Lab.EF.MVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            CategoriesLogic categoriesLogic = new CategoriesLogic();
+
+            List<CategoriesView> categories = categoriesLogic.GetAll().Select(c => new CategoriesView {
+                Id = c.CategoryID,
+                CategoryName = c.CategoryName,
+                Description = c.Description
+            }).ToList();
+
+            return View(categories);
+        }
+    }
+}
