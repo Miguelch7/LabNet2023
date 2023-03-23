c5c7e39bdc950456ba525c97a89514c8821f23a7
Miguelch7 <chanampamiguel@gmail.com>
2023-03-23 17:47:19 -0300

    Mostrando mensaje de confirmación antes de eliminar categoría

---
+++ Lab.EF/Lab.EF.MVC/Scripts/script.js
@@ -0,0 +1,15 @@
+﻿function mostrarAlerta(controller, action, id, message) {
+    Swal.fire({
+        title: message,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#dc3545',
+        confirmButtonText: 'Si, eliminar!',
+        cancelButtonColor: '#6e7881',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = `${controller}/${action}/${id}`;
+        }
+    })
+}
