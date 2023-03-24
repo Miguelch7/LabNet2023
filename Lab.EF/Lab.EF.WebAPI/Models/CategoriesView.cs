8973523045ce742fa76dd830699a6e16d4b269f7
Miguelch7 <chanampamiguel@gmail.com>
2023-03-24 13:23:06 -0300

    Creando view model categoria

---
+++ Lab.EF/Lab.EF.WebAPI/Models/CategoriesView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab.EF.WebAPI.Models
+{
+    public class CategoriesView
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la categoría es obligatorio")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre de la categoría debe contener al menos 3 caracteres")]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
