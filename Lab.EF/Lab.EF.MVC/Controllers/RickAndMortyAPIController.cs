71613d820f32c3100e13b7e62d377a7bc39b5dbd
Miguelch7 <chanampamiguel@gmail.com>
2023-03-25 18:22:07 -0300

    Ver detalle personaje de Rick y Morty

--- Lab.EF/Lab.EF.MVC/Controllers/RickAndMortyAPIController.cs
+++ Lab.EF/Lab.EF.MVC/Controllers/RickAndMortyAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
@@ -24,11 +25,31 @@ namespace Lab.EF.MVC.Controllers
         {
             HttpResponseMessage responseMessage = await _httpClient.GetAsync("character");
 
+            if (!responseMessage.IsSuccessStatusCode) return RedirectToAction("Error", new { message = "Hubo un error al intentar cargar los personajes." });
+
             RickAndMortyAPI response = await responseMessage.Content.ReadAsAsync<RickAndMortyAPI>();
 
             List<Character> characters = response.Results;
             
             return View(characters);
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            HttpResponseMessage responseMessage = await _httpClient.GetAsync($"character/{id}");
+
+            if (responseMessage.StatusCode == HttpStatusCode.NotFound) return RedirectToAction("Error", new { message = $"No existe un personaje con el id {id}." });
+            if (responseMessage.StatusCode == HttpStatusCode.InternalServerError) return RedirectToAction("Error", new { message = "Ha ocurrido un error en el servidor." });
+
+            Character character = await responseMessage.Content.ReadAsAsync<Character>();
+
+            return View(character);
+        }
+
+        public ActionResult Error(string message)
+        {
+            ViewBag.Message = message;
+            return View();
         }
     }
 }
